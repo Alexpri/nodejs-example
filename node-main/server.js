be0f1b2b131dfeefb55e3a2b480c171cc467d80e
Alexpri <Alexpri@github.com>
2016-03-12 17:08:16 +0200

    moduls

---
+++ node-main/server.js
@@ -0,0 +1,16 @@
+var user = require('./user');
+
+
+function run () {
+    var vasya = new user.User("Вася");
+    var petya = new user.User("Петя");
+
+    vasya.hello(petya);
+}
+
+
+if (module.parent) {
+    exports.run = run;
+} else {
+    run();
+}
